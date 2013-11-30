819a2cedac700950dc0695bd44891c6122d7565e
Mario Garcia <mario.ggar@gmail.com>
2013-11-30 11:45:42 +0100

    Fixed checking Im4Java is present. CopyPaste mistake :(

--- src/test/groovy/benchmark/Im4JavaNotAvailable.groovy
+++ src/test/groovy/benchmark/Im4JavaNotAvailable.groovy
@@ -16,6 +16,8 @@
  */
 package benchmark
 
+import groovy.util.logging.Log
+
 /**
  * This closure checks Im4java is not available. This closure
  * will be used by Spock's @IgnoreIf annotation to ignore
@@ -23,7 +25,10 @@ package benchmark
  *
  * @author Mario Garcia
  */
+@Log
 class Im4JavaNotAvailable extends Closure<Boolean> {
+
+    static final Integer ABNORMAL_TERMINATION = 1
 
     Im4JavaNotAvailable(Object owner, Object delegate) {
         super(owner, delegate)
@@ -31,13 +36,25 @@ class Im4JavaNotAvailable extends Closure<Boolean> {
 
     Boolean doCall() {
 
-        def process = ["convert","--version"].execute()
+        def result = ABNORMAL_TERMINATION
 
-        process.waitForProcessOutput()
+        try {
+
+            def process = ["convert","--version"].execute()
+            def out = new StringBuilder()
+            def err = new StringBuilder()
+
+            process.waitForProcessOutput(out, err)
+            result = process.exitValue()
+
+        } catch (e) {
+            log.info(e.message)
+        }
+
      /* If 0 indicates normal termination, in this case
         Im4java is not available if the method returns
         true */
-        return process.exitValue() == 0 ? false : true
+        return result == 0 ? false : true
 
     }
 
